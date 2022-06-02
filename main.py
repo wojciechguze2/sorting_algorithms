159640979740c71867f0093185814d0607a62939
wojciechguze2 <wojciech.guze@coloursfactory.pl>
2022-06-02 15:58:18 +0200

    moved sorting algorithms to class

--- main.py
+++ main.py
@@ -1,129 +1,117 @@
-from random import randint
-
-
 # naive sorting algorithms
 
+class Sort:
+    """
+    def __init__(self, list_of_numbers):
+        self.list_of_numbers = list_of_numbers
+    """
 
-def selection_sort(list_of_numbers: list) -> list:
-    # iterating over unsorted part of list to find min value and put it to the end of sorted part
-    list_of_numbers_len = len(list_of_numbers)
+    def selection_sort(self, list_of_numbers: list) -> list:
+        # iterating over unsorted part of list to find min value and put it to the end of sorted part
+        list_of_numbers_len = len(list_of_numbers)
 
-    for i in range(list_of_numbers_len):
-        minimum_value_j_index = i
+        for i in range(list_of_numbers_len):
+            minimum_value_j_index = i
 
-        for j in range(i + 1, list_of_numbers_len):
-            if list_of_numbers[j] < list_of_numbers[minimum_value_j_index]:
-                minimum_value_j_index = j
+            for j in range(i + 1, list_of_numbers_len):
+                if list_of_numbers[j] < list_of_numbers[minimum_value_j_index]:
+                    minimum_value_j_index = j
 
-        swap(list_of_numbers, i, minimum_value_j_index)
+            self.swap(list_of_numbers, i, minimum_value_j_index)
 
-    return list_of_numbers
-
-
-def insertion_sort(list_of_numbers: list, n=None) -> list:
-    # comparing n and n + 1 and swapping, calling self recursively n times (n - length of list)
-    list_of_numbers_len = len(list_of_numbers)
-
-    if n is None:
-        n = list_of_numbers_len - 1
-
-    if n > 0:
-        insertion_sort(list_of_numbers, n - 1)
-
-    for i in range(list_of_numbers_len - 1):
-        if list_of_numbers[i] > list_of_numbers[i + 1]:
-            swap(list_of_numbers, i, i + 1)
-
-    return list_of_numbers
-
-
-def bubble_sort(list_of_numbers: list) -> list:
-    # comparing n and n + 1 until list is not sorted
-    list_of_numbers_len = len(list_of_numbers)
-
-    for i in range(list_of_numbers_len):
-        for j in range(list_of_numbers_len - i - 1):
-            if list_of_numbers[j] > list_of_numbers[j + 1]:
-                swap(list_of_numbers, j, j + 1)
-
-    return list_of_numbers
-
-
-# divide and conquer sorting algorithms
-
-
-def quick_sort(list_of_numbers: list) -> list:
-    # (recursively) sorting numbers on the left of pivot and right
-    # removes duplicates todo: fix this
-    list_of_numbers_len = len(list_of_numbers)
-
-    if list_of_numbers_len <= 1:
         return list_of_numbers
 
-    pivot = list_of_numbers[list_of_numbers_len // 2]
+    def insertion_sort(self, list_of_numbers: list, n=None) -> list:
+        # comparing n and n + 1 and swapping, calling self recursively n times (n - length of list)
+        list_of_numbers_len = len(list_of_numbers)
 
-    left = [n for n in list_of_numbers if n < pivot]
-    right = [n for n in list_of_numbers if n > pivot]
+        if n is None:
+            n = list_of_numbers_len - 1
 
-    return quick_sort(left) + [pivot] + quick_sort(right)
+        if n > 0:
+            self.insertion_sort(list_of_numbers, n - 1)
 
+        for i in range(list_of_numbers_len - 1):
+            if list_of_numbers[i] > list_of_numbers[i + 1]:
+                self.swap(list_of_numbers, i, i + 1)
 
-def merge_sort(list_of_numbers: list) -> list:
-    # split list into left and right half, do the same on each of them, sort, then merge into 1
-    list_of_numbers_len = len(list_of_numbers)
+        return list_of_numbers
 
-    if list_of_numbers_len > 1:
-        middle = list_of_numbers_len // 2
+    def bubble_sort(self, list_of_numbers: list) -> list:
+        # comparing n and n + 1 until list is not sorted
+        list_of_numbers_len = len(list_of_numbers)
 
-        left = list_of_numbers[0:middle]
-        right = list_of_numbers[middle:]
+        for i in range(list_of_numbers_len):
+            for j in range(list_of_numbers_len - i - 1):
+                if list_of_numbers[j] > list_of_numbers[j + 1]:
+                    self.swap(list_of_numbers, j, j + 1)
 
-        merge_sort(left)
-        merge_sort(right)
+        return list_of_numbers
 
-        lc, rc, i = 0, 0, 0
-        len_left, len_right = len(left), len(right)
+    # divide and conquer sorting algorithms
 
-        while lc < len_left and rc < len_right:
-            if left[lc] < right[rc]:
+    def quick_sort(self, list_of_numbers: list) -> list:
+        # (recursively) sorting numbers on the left of pivot and right
+        # removes duplicates todo: fix this
+        list_of_numbers_len = len(list_of_numbers)
+
+        if list_of_numbers_len <= 1:
+            return list_of_numbers
+
+        pivot = list_of_numbers[list_of_numbers_len // 2]
+
+        left = [n for n in list_of_numbers if n < pivot]
+        right = [n for n in list_of_numbers if n > pivot]
+
+        return self.quick_sort(left) + [pivot] + self.quick_sort(right)
+
+    def merge_sort(self, list_of_numbers: list) -> list:
+        # split list into left and right half, do the same on each of them, sort, then merge into 1
+        list_of_numbers_len = len(list_of_numbers)
+
+        if list_of_numbers_len > 1:
+            middle = list_of_numbers_len // 2
+
+            left = list_of_numbers[0:middle]
+            right = list_of_numbers[middle:]
+
+            self.merge_sort(left)
+            self.merge_sort(right)
+
+            lc, rc, i = 0, 0, 0
+            len_left, len_right = len(left), len(right)
+
+            while lc < len_left and rc < len_right:
+                if left[lc] < right[rc]:
+                    list_of_numbers[i] = left[lc]
+                    lc += 1
+                else:
+                    list_of_numbers[i] = right[rc]
+                    rc += 1
+
+                i += 1
+
+            while lc < len_left:
                 list_of_numbers[i] = left[lc]
                 lc += 1
-            else:
+                i += 1
+
+            while rc < len_right:
                 list_of_numbers[i] = right[rc]
                 rc += 1
+                i += 1
 
-            i += 1
+            return list_of_numbers
 
-        while lc < len_left:
-            list_of_numbers[i] = left[lc]
-            lc += 1
-            i += 1
+    def heap_sort(self, list_of_numbers: list) -> list:
+        return []
 
-        while rc < len_right:
-            list_of_numbers[i] = right[rc]
-            rc += 1
-            i += 1
+    # helping algorithms
 
-        return list_of_numbers
+    @staticmethod
+    def swap(list_of_numbers: list, index_1: int, index_2: int):
+        list_of_numbers[index_1], list_of_numbers[index_2] = list_of_numbers[index_2], list_of_numbers[index_1]
 
 
-def heap_sort(list_of_numbers: list) -> list:
-    return []
-
-
-# helping algorithms
-
-def swap(list_of_numbers: list, index_1: int, index_2: int):
-    list_of_numbers[index_1], list_of_numbers[index_2] = list_of_numbers[index_2], list_of_numbers[index_1]
-
-
-test_list = [
-    randint(0, 15)
-    for _ in range(11)
-]
-
-print(selection_sort(test_list))
-print(insertion_sort(test_list))
-print(bubble_sort(test_list))
-print(quick_sort(test_list))
-print(merge_sort(test_list))
+sorting = Sort()
+print(sorting.quick_sort([1, 2, 3]))
